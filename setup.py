da3ab4ecbcfc15f48d546db8784ff61cc3aad1f0
Sh4d <aniellob@gmail.com>
2014-07-10 16:31:15 +0200

    Update setup.py
    let's try..

--- setup.py
+++ setup.py
@@ -1,38 +1,14 @@
-#!/usr/bin/env python
 
-#  Copyright (c) 2010 Franz Allan Valencia See
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
 
 
 """Setup script for Robot's DatabaseLibrary distributions"""
 
 import sys, os
 from setuptools import setup, find_packages
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 
-install_requires = [
-	'robotframework==2.8.4',
-	]
-
-setup(name         = 'robotframework-databaselibrary',
-      version      = '0.6',
-      description  = 'Database utility library for Robot Framework',
-      author       = 'Franz Allan Valencia See',
-      author_email = 'franz.see@gmail.com',
-      url          = 'https://github.com/franz-see/Robotframework-Database-Library',
-      package_dir  = { '' : 'src'},
-      packages     = ['DatabaseLibrary'],
-      install_require = install_requires
-      )
-
+setup(
+    name="robotframework-databaselibrary",
+    version="0.6",
+    packages=find_packages(),
+    install_requires=['robotframework']
+)
